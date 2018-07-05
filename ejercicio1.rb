3feb152ec24eef54b0c150038ae05a5fbf6bdab6
paskuvan <majopaskuvan@gmail.com>
2018-07-05 12:01:44 -0400

    solved

---
+++ ejercicio1.rb
@@ -0,0 +1,30 @@
+def archivo(string1, string2)
+  file = File.open('ciudades.html', 'w')
+  file.puts "<p> #{string1} </p>"
+  file.puts "<p> #{string2} </p>"
+  file.close
+end
+
+archivo('New York', 'USA')
+
+def add_list(array)
+  
+  file = File.open('ciudades.html', 'a') #abre el archivo ciudades.html
+  file.puts '<ul>' #lista
+  array.each { |foo| file.puts "<li> #{foo} </li>" }
+  file.puts '</ul>'
+  return if array.count.zero? #contador de arrays. en ese caso, 5 ciudades
+  file.close #cierra el archivo
+end
+
+ciudad = %w[Bronx, Queens, Brooklyn, Syracuse, Manhattan] #muestra las ciudades en una lista
+
+add_list(ciudad)
+
+def agrega_color(color) #agrega color
+  file = File.open('ciudades.html', 'r')
+  lines = file.readlines.map(&:length)
+  print lines
+end
+
+agrega_color('color')
